6aee8fa3f105a0ac3267fcfb8a71b5d9ff1ecf50
Christoph Schöpf <ch.schoepf1@hotmail.com>
2015-10-09 01:07:10 +0200

    init repo, start first task

---
+++ 01/bal.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Balthazar</title>
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/theme.css">
+<script language="javascript" type="text/javascript" src="script/control.js"></script>
+</head>
+<body>
+
+<h1 id="header2">Balthazar</h1>
+<table id="table">
+	<tr>
+		<td id="name1">Balthazar</td>
+  	</tr>
+  	<tr>
+  		<td>Huber</td>
+  	</tr>
+  	<tr>
+  		<td>Informatik</td>
+  	</tr>
+</table>
+<p>
+<table id="table">
+  	<tr> 
+  		<td><button onclick="backOverview()">Back to Overview</button></td>
+  		<td><button onclick="fanbal()">Become a fan</button></td>
+	</tr>
+</table>
+
+</body>
+</html>
---
+++ 01/config/README.txt
@@ -0,0 +1,7 @@
+httpd.conf -> /etc/httpd/conf
+ssl.conf -> /etc/httpd/conf.d
+php.conf -> /etc/httpd/conf.d
+
+server.crt -> /etc/pki/tls/certs
+server.csr -> /etc/pki/tls/private
+server.key -> /etc/pki/tls/private
